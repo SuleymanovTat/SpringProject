cbb0fd584db3087335e2e77568a9b33699e44dba
Сулейманов Ильмир Анварович <isuleimanov@srt-llc.ru>
2025-01-09 17:24:03 +0300

    2.8 Join Point

--- src/main/java/ru/suleymanovtat/aop/aspects/LoggerAspect.java
+++ src/main/java/ru/suleymanovtat/aop/aspects/LoggerAspect.java
@@ -1,8 +1,11 @@
 package ru.suleymanovtat.aop.aspects;
 
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
+import org.aspectj.lang.reflect.MethodSignature;
+import ru.suleymanovtat.model.bean.Book;
 
 @Aspect
 public class LoggerAspect {
@@ -12,7 +15,24 @@ public class LoggerAspect {
     }
 
     @Before("allMethods()")
-    public void beforeAllMethodsAdvice() {
-        System.out.println("beforeAllMethodsAdvice: Logger");
+    public void beforeAllMethodsAdvice(JoinPoint joinPoint) {
+        MethodSignature signature = (MethodSignature) joinPoint.getSignature();
+        // Получение имени метода
+        String methodName = signature.getName();
+        // Получение значений параметров
+        Object[] args = joinPoint.getArgs();
+        // Вывод информации о параметрах и их значениях
+        System.out.println("Method Name: " + methodName);
+        for (Object arg : args) {
+            if (arg instanceof Book) {
+                System.out.println("______________________________");
+                System.out.println("______________________________");
+                Book book = (Book) arg;
+                System.out.println("___Parameter Value: " + book.getName() + "___");
+                System.out.println("______________________________");
+                System.out.println("______________________________");
+            }
+        }
+        System.out.println("beforeAllMethodsAdvice: Logger " + methodName);
     }
 }
--- src/main/java/ru/suleymanovtat/model/bean/Book.java
+++ src/main/java/ru/suleymanovtat/model/bean/Book.java
@@ -2,6 +2,8 @@ package ru.suleymanovtat.model.bean;
 
 import org.springframework.beans.factory.annotation.Value;
 
+import java.util.Objects;
+
 public class Book {
 
     @Value("${user.name}")
@@ -9,5 +11,17 @@ public class Book {
 
     public String getName() {
         return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return Objects.equals(name, book.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(name);
     }
 }
