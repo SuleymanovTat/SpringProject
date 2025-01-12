e47781510379a545e97528a8ee2f86282f584ac9
Сулейманов Ильмир Анварович <isuleimanov@srt-llc.ru>
2025-01-12 13:29:35 +0300

    2.11 Advice тип @After

--- src/main/java/ru/suleymanovtat/aop/aspects/LoggerStoreAspect.java
+++ src/main/java/ru/suleymanovtat/aop/aspects/LoggerStoreAspect.java
@@ -1,6 +1,7 @@
 package ru.suleymanovtat.aop.aspects;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.AfterThrowing;
 import org.aspectj.lang.annotation.Aspect;
@@ -20,8 +21,15 @@ public class LoggerStoreAspect {
     @AfterThrowing(pointcut = "execution(* ru.suleymanovtat.aop.Store.getListOfMeals())", throwing = "error")
     public void afterThrowingGetListOfMealsAdvice(JoinPoint joinPoint, Throwable error) {
         System.out.println("_____________");
-        System.out.println("afterThrowingGetListOfMealsAdvice: "+joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
-        System.out.println("afterThrowingGetListOfMealsAdvice: "+error.getMessage());
+        System.out.println("afterThrowingGetListOfMealsAdvice: " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
+        System.out.println("afterThrowingGetListOfMealsAdvice: " + error.getMessage());
+        System.out.println("_____________");
+    }
+
+    @After(value = "execution(* ru.suleymanovtat.aop.Store.getListOfMeals())")
+    public void afterGetListOfMealsAdvice(JoinPoint joinPoint) {
+        System.out.println("_____________");
+        System.out.println("after: " + joinPoint.getSignature().getDeclaringTypeName() + "." + joinPoint.getSignature().getName());
         System.out.println("_____________");
     }
 }
